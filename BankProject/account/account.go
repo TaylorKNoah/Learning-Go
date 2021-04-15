2166d2c5cea6eeb9672defbf55b0c41185ad7609
TaylorKNoah <73961491+TaylorKNoah@users.noreply.github.com>
2021-04-14 19:59:52 -0700

    add account methods, balance, withdraw. error chekcing

--- BankProject/account/account.go
+++ BankProject/account/account.go
@@ -1,5 +1,7 @@
 package account
 
+import "errors"
+
 //Account Struct
 type account struct {
 	owner   string
@@ -10,4 +12,23 @@ type account struct {
 func NewAccount(owner string) *account {
 	new_Account := account{owner: owner, balance: 0}
 	return &new_Account
+}
+
+// Deposit x amount to the account
+func (a *account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Return balance
+func (a account) Balance() int {
+	return a.balance
+}
+
+//withdraw from your account
+func (a *account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errors.New("can't withdraw. you are poor")
+	}
+	a.balance -= amount
+	return nil
 }
--- BankProject/main.go
+++ BankProject/main.go
@@ -7,6 +7,18 @@ import (
 )
 
 func main() {
+
+	//This block creats an account, adds to the balance, and displays the balance
 	my_account := account.NewAccount("taylor")
-	fmt.Println(my_account)
+	my_account.Deposit(10)
+	fmt.Println(my_account.Balance())
+
+	//This block trys to withdraw from the balance
+	//If the withdraw is greater than the balance an error is returned and displayed
+	//Displays balance
+	errWithdraw := my_account.Withdraw(20)
+	if errWithdraw != nil {
+		fmt.Println(errWithdraw)
+	}
+	fmt.Println(my_account.Balance())
 }
