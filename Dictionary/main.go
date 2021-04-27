2a1f4ec8812194beb219acdc79272890235dd288
Taylor <taylorknoah@gmail.com>
2021-04-26 21:11:38 -0700

    add dictionary functions(update, delete)

--- Dictionary/main.go
+++ Dictionary/main.go
@@ -9,11 +9,51 @@ import (
 func main() {
 	dictionary := mydict.Dictionary{"First": "First Word"}
 
-	def, err := dictionary.Search("Second")
+	word := "LET'S"
+	definition := "not go"
 
+	//test add fucntion
+	err := dictionary.Add(word, definition)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 	} else {
-		fmt.Println(def)
+		println("INIT:", word, definition)
 	}
+
+	word = "LET'S"
+	//test Search
+	def2, err2 := dictionary.Search(word)
+	if err2 != nil {
+		fmt.Println("\n", err2)
+	} else {
+		fmt.Println("SEARCH: ", word, " | FOUND: ", def2)
+	}
+
+	update := "GO!!"
+	//test update
+	err3 := dictionary.Update(word, update)
+	if err3 != nil {
+		fmt.Println('\n', err3)
+	} else {
+		fmt.Println("UPDATE: ", word, update)
+	}
+
+	err4 := dictionary.Update("Fanny-Packs-Are-Cool", update)
+	//test update error
+	if err4 != nil {
+		fmt.Println(err4)
+	} else {
+		fmt.Println("UPDATE: ", word, update)
+	}
+
+	//test delete function
+	for i := 0; i < 2; i++ {
+		err5 := dictionary.Delete(word)
+		if err5 != nil {
+			fmt.Println(err5)
+		} else {
+			fmt.Println("DELETE SUCCESSFUL")
+		}
+	}
+
 }
--- Dictionary/mydict/mydict.go
+++ Dictionary/mydict/mydict.go
@@ -2,9 +2,15 @@ package mydict
 
 import "errors"
 
+//dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("err: not found")
+var (
+	errNotFound     = errors.New("err: not found")
+	errWordExists   = errors.New("err: word exists")
+	errCannotUpdate = errors.New("err: cannot update non-existing word")
+	errCannotDelete = errors.New("error: cannot delete word. does not exist")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
@@ -14,4 +20,50 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return "", errNotFound
+}
+
+//Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+
+	//search for word, catch error
+	_, err := d.Search(word)
+
+	//check error and if not found
+	//ret error other wise
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errNotFound
+	}
+
+	return nil
+
+}
+
+//Update a words definition
+func (d Dictionary) Update(word, defin string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = defin
+	case errNotFound:
+		return errCannotUpdate
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		delete(d, word)
+	case errNotFound:
+		return errCannotDelete
+	}
+
+	return nil
 }
