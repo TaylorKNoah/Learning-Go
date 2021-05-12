80b8a54686b9c258f1f80d13461379f31b0718e2
Taylor <taylorknoah@gmail.com>
2021-05-11 18:01:37 -0700

    add concurrent url checker

---
+++ URL_Checker_Fast/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var (
+	results          = make(map[string]string)
+	errRequestFailed = errors.New("request failed")
+)
+
+type requestResult struct {
+	url    string
+	status string
+}
+
+func main() {
+
+	c := make(chan requestResult)
+	results := make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com",
+		"https://www.google.com",
+		"https://github.com",
+		"https://amazon.com",
+		"https://www.reddit.com",
+		"https://www.soundcloud.com",
+		"https://www.linkedon.com", //purposefully incorrect to test error codes
+	}
+
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(status, url)
+	}
+
+}
+
+//hit URL, only sends through channel
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "SUCCESS"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+
+	c <- requestResult{url: url, status: status}
+}
